32d66fc45f42d6bf6cc0aeff219cdf948fd758a8
nowehner <97761745+nowehner@users.noreply.github.com>
2024-04-04 22:03:04 -0500

    done

--- src/main/java/edu/iu/habahram/ducksservice/controllers/AuthenticationController.java
+++ src/main/java/edu/iu/habahram/ducksservice/controllers/AuthenticationController.java
@@ -2,15 +2,39 @@ package edu.iu.habahram.ducksservice.controllers;
 
 import edu.iu.habahram.ducksservice.model.Customer;
 import edu.iu.habahram.ducksservice.repository.CustomerRepository;
+import edu.iu.habahram.ducksservice.security.TokenService;
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
 public class AuthenticationController {
     CustomerRepository customerRepository;
+    private final AuthenticationManager authenticationManager;
+
+    private final TokenService tokenService;
     public AuthenticationController(CustomerRepository
-                                            customerRepository) {
+                                            customerRepository, AuthenticationManager authenticationManager, TokenService tokenService) {
         this.customerRepository = customerRepository;
+        this.authenticationManager = authenticationManager;
+        this.tokenService = tokenService;
+    }
+
+
+    @PostMapping("/signin")
+    public String login(@RequestBody Customer customer){
+        Authentication authentication = authenticationManager
+                .authenticate(
+                        new UsernamePasswordAuthenticationToken(
+                                customer.username()
+                                , customer.password()));
+
+        return tokenService.generateToken(authentication);
+
+
     }
     @PostMapping("/signup")
     public void signup(@RequestBody Customer customer) {
---
+++ src/main/java/edu/iu/habahram/ducksservice/security/Jwks.java
@@ -0,0 +1,24 @@
+package edu.iu.habahram.ducksservice.security;
+
+import java.security.KeyPair;
+import com.nimbusds.jose.jwk.RSAKey;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.UUID;
+
+public class Jwks {
+
+
+        public static RSAKey generateRsa() {
+            KeyPair keyPair = KeyGeneratorUtils.generateRsaKey();
+            RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+            RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+            return new RSAKey.Builder(publicKey)
+                    .privateKey(privateKey)
+                    .keyID(UUID.randomUUID().toString())
+                    .build();
+
+
+        }
+    }
+
